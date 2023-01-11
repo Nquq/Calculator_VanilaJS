9b6cb217dc15bbb0892ea9a1b1630cdf922e2c2a
Nquq <113373929+Nquq@users.noreply.github.com>
2023-01-12 01:03:09 +0300

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,133 @@
+const digit = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+const action = ['-', '+', 'X', '/'];
+
+const output = document.querySelector('.output-screen');
+const clearBtn = document.querySelector('.clear');
+const buttons = document.querySelectorAll('.btn');
+
+output.textContent = 0;
+
+let firstNumber = '';
+let secondNumber = '';
+let sign = '';
+let isCalculated = false;
+
+clearBtn.addEventListener('click', (event) => {
+    clearOutput();
+});
+
+buttons.forEach((button) => {
+    button.addEventListener('click', (event) => {
+        const key = event.target.textContent;
+
+        if (key === '+/-') {
+            if (!firstNumber) {
+                firstNumber = '-0.';
+                output.textContent = firstNumber;
+            } else if (!secondNumber && !sign) {
+                if (firstNumber.includes('-')) return;
+                firstNumber = '-' + firstNumber;
+                output.textContent = firstNumber;
+            } else {
+                if (secondNumber.includes('-')) return;
+                if (!secondNumber) {
+                    secondNumber = '-' + secondNumber;
+                    output.textContent = secondNumber;
+                    return;
+                }
+                secondNumber = '-' + secondNumber;
+                output.textContent = secondNumber;
+            }
+        }
+
+        if (key === '%') {
+            if (!firstNumber) {
+                return;
+            }
+            if (!secondNumber && !sign) {
+                firstNumber /= 100;
+                output.textContent = firstNumber;
+            }
+            if (sign === '+' || sign === '-') {
+                secondNumber = (firstNumber * secondNumber) / 100;
+                output.textContent = secondNumber;
+            }
+            if (sign === 'X' || sign === '/') {
+                secondNumber /= 100;
+                output.textContent = secondNumber;
+            }
+        }
+
+        if (key === '.') {
+            if (!firstNumber) {
+                firstNumber = '0.';
+                output.textContent = firstNumber;
+            } else if (!secondNumber && !sign) {
+                if (firstNumber.includes('.')) return;
+                firstNumber += key;
+                output.textContent = firstNumber;
+            } else {
+                if (secondNumber.includes('.')) return;
+                if (!secondNumber) {
+                    secondNumber = '0.';
+                    output.textContent = secondNumber;
+                    return;
+                }
+                secondNumber += key;
+                output.textContent = secondNumber;
+            }
+        }
+
+        if (digit.includes(key)) {
+            if (isCalculated) {
+                clearOutput();
+            }
+            if (!secondNumber && !sign) {
+                firstNumber += key;
+                output.textContent = firstNumber;
+            } else {
+                secondNumber += key;
+                output.textContent = +secondNumber;
+            }
+        }
+
+        if (action.includes(key)) {
+            isCalculated = false;
+            sign = key;
+            output.textContent = '';
+        }
+
+        if (key === '=') {
+            if (!secondNumber) secondNumber = +firstNumber;
+            switch (sign) {
+                case '+':
+                    firstNumber = +firstNumber + +secondNumber;
+                    break;
+                case '-':
+                    firstNumber = +firstNumber - +secondNumber;
+                    break;
+                case 'X':
+                    firstNumber = +firstNumber * +secondNumber;
+                    break;
+                case '/':
+                    firstNumber = +firstNumber / +secondNumber;
+                    break;
+                default:
+                    break;
+            }
+            if (secondNumber) secondNumber = '';
+            isCalculated = true;
+            output.textContent = +firstNumber.toFixed(2);
+        }
+
+        console.log(firstNumber, sign, secondNumber);
+    });
+});
+
+function clearOutput() {
+    firstNumber = '';
+    secondNumber = '';
+    sign = '';
+    isCalculated = false;
+    output.textContent = 0;
+}
